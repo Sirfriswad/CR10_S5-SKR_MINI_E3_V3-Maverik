79745103166cdbe9f212ce91c892ab075af72aa0
3DPrinter <3dprinter@maverik.com>
2022-08-17 12:17:41 -0600

    Invert the x axis motor direction

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1632,7 +1632,7 @@
 // @section machine
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
-#define INVERT_X_DIR false
+#define INVERT_X_DIR true // Maverik CR-10 S5, invert the x direction
 #define INVERT_Y_DIR true
 #define INVERT_Z_DIR false
 //#define INVERT_I_DIR false
