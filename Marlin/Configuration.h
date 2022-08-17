95646ba0ec5c56db939b6840aa83bc9727d0662d
3DPrinter <3dprinter@maverik.com>
2022-08-17 12:46:45 -0600

    BL Touch probe a 5x5 grid

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1941,7 +1941,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 3
+  #define GRID_MAX_POINTS_X 5 // Maverik BLTouch probe 5x5 grid on the bed
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
