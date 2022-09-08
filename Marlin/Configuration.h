d6c54e00a988ab636636372e1784fe23689d8cb9
3dPrinter <3dprinter@maverik.com>
2022-09-08 12:48:49 -0600

    Max Z_POS 450

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1697,7 +1697,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 500 // Maverik CR10-S5
+#define Z_MAX_POS 450 // Maverik CR10-S5
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
